55c99ebccca2ecfe08476f81f769d704ef766fbb
Mikael Medeiros <mikaeltruth@gmail.com>
2022-04-30 22:31:46 -0300

    allowing cors

--- src/main/java/com/api/moondb/context/auth/controller/LoginController.java
+++ src/main/java/com/api/moondb/context/auth/controller/LoginController.java
@@ -5,6 +5,7 @@ import com.api.moondb.context.auth.model.response.LoginResponse;
 import com.api.moondb.context.designermoodb.services.LoginService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
+@CrossOrigin
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/login")
